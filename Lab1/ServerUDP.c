c9fe5e551bfcd8512b2645a855f4e21fa0940f33
Ian <Typhoan@users.noreply.github.com>
2014-09-21 17:50:51 -0500

    got it to send data

--- Lab1/ServerUDP.c
+++ Lab1/ServerUDP.c
@@ -15,191 +15,182 @@
 
 #define MAXBUFLEN 1029
 
-struct rPacket {
-uint16_t length;
-uint16_t id;
-uint8_t op;
-char* str;
-};
 
-typedef rPacket rPacket_t;
-
-struct nvPacket {
+typedef struct request {
    uint16_t length;
    uint16_t id;
-   uint16_t numVowel;
-};
+   uint8_t op;
+   char* message;
+} request_t;
 
-typedef nvPacket nvPacket_t;
-
-struct dvPacket {
+typedef struct dvResponse {
    uint16_t length;
    uint16_t id;
-   char* dv;
-};
+   char* message;
+} dvResponse_t;
 
-typedef dvPacket dvPacket_t;
+typedef struct nvResponse {
+   uint16_t length;
+   uint16_t id;
+   uint16_t numV;
+} nvResponse_t;
 
-uint16_t vLength(char* str) {
-	int i,numVowel = 0;
-
-   for(i; i < sizeOf(str); i++) {
-    if(check_vowel(str[i]) == 1) {       //is a vowel
-      numVowel++;
-    }
-  }
-}
-
-char* disemvowel(char* str){
-    char* dv;
-    int i,j = 0;
-
-    for(i; i < sizeOf(str); i++) {
-    if(check_vowel(str[i]) == 0) {       //not a vowel
-      dv[j] = str[i];
-      j++;
-    }
-  }
-
-  return dv;
-}
-
-int check_vowel(char c)
+int isVowel(char c)
 {
-  switch(c) {
-    case 'a':
-    case 'A':
-    case 'e':
-    case 'E':
-    case 'i':
-    case 'I':
-    case 'o':
-    case 'O':
-    case 'u':
-    case 'U':
-      return 1;
-    default:
-      return 0;
-  }
+   switch(c) {
+      case 'a':
+      case 'A':
+      case 'e':
+      case 'E':
+      case 'i':
+      case 'I':
+      case 'o':
+      case 'O':
+      case 'u':
+      case 'U':
+         return 1;
+      default:
+         return 0;
+   }
 }
 
+uint16_t numVowels(char* str, int len) {
+   int i = 0; 
+	uint16_t count = 0;
+
+   for (i = 0; i < len; i++) {
+      if(isVowel(str[i])) {
+         count++;
+      }
+
+   }
+
+   return count;
+}
+
+void disemvowel(char* source, char* dest, int len) {
+   int i,j = 0;
+
+   for (i = 0; i < len; i++) {
+      if(!isVowel(source[i])) {
+         dest[j++] = source[i];
+      }
+
+   }
+}
 
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
-	if (sa->sa_family == AF_INET) {
-		return &(((struct sockaddr_in*)sa)->sin_addr);
-	}
+   if (sa->sa_family == AF_INET) {
+      return &(((struct sockaddr_in*)sa)->sin_addr);
+   }
 
-	return &(((struct sockaddr_in6*)sa)->sin6_addr);
+   return &(((struct sockaddr_in6*)sa)->sin6_addr);
 }
 
-int main(int argc, char *argv)
+int main(int argc, char* argv[])
 {
-	int sockfd;
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytesRecv, numbytesSent;
-	struct sockaddr_storage their_addr;
-	char buf[MAXBUFLEN];
-	socklen_t addr_len;
-	char their_addr_str[INET6_ADDRSTRLEN];
-	rPacket_t receiveData;
-   int myPort;
+   int packetSize = 0;
+   int sockfd;
+   struct addrinfo hints, *servinfo, *p;
+   int rv;
+   int numbytesRecv, numbytesSent;
+   struct sockaddr_storage their_addr;
+   char buf[MAXBUFLEN];
+   socklen_t addr_len;
+   char their_addr_str[INET6_ADDRSTRLEN];
+   request_t request;
    char* sendData = NULL;
+   char message[MAXBUFLEN];
+
 
    if (argc != 2)
       exit(1);
-   else
-      myPort = (int)argv[1];
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
-	hints.ai_socktype = SOCK_DGRAM;
-	hints.ai_flags = AI_PASSIVE; // use my IP
+   memset(&hints, 0, sizeof hints);
+   hints.ai_family = AF_UNSPEC; // set to AF_INET to force IPv4
+   hints.ai_socktype = SOCK_DGRAM;
+   hints.ai_flags = AI_PASSIVE; // use my IP
 
-	if ((rv = getaddrinfo(NULL, myPort, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
+   if ((rv = getaddrinfo(NULL, argv[1], &hints, &servinfo)) != 0) {
+      fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+      return 1;
+   }
 
-	// loop through all the results and bind to the first we can
-	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
-				p->ai_protocol)) == -1) {
-			perror("listener: socket");
-			continue;
-		}
-
-		if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
-			close(sockfd);
-			perror("listener: bind");
-			continue;
-		}
-
-		break;
-	}
-
-	if (p == NULL) {
-		fprintf(stderr, "listener: failed to bind socket\n");
-		return 2;
-	}
-
-	freeaddrinfo(servinfo);
-
-	while(1) {
-		printf("listener: waiting to recvfrom...\n");
-
-		addr_len = sizeof their_addr;
-		if ((numbytesRecv = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
-			(struct sockaddr *)&their_addr, &addr_len)) ==-1) 			{
-			perror("recvfrom");
-			exit(1);
-		}
-
-		printf("listener: got packet from %s\n",
-			inet_ntop(their_addr.ss_family,
-				get_in_addr((struct sockaddr *)&their_addr),
-				their_addr_str, sizeof their_addr_str));
-
-		receiveData.length = buf[0];
-		receiveData.length = test.length << 8;
-		receiveData.length = test.length | buf[1];
-		receiveData.id = buf[2];
-		receiveData.id = test.id << 8;
-		receiveData.id = test.id | buf[3];
-		receiveData.op = buf[4];
-		int j = 0;
-		for (int i = 5; i <sizeOf(buf); i++) {
-			receiveData.str[j] = buf[i];
-		}
-
-		if (receiveData.op == 0x55){
-         nvPacket_t sendPacket;
-         sendPacket.length = receiveData.length
-         sendPacket.id = receiveData.id;
-			sendPacket.numVowel = vLength(receiveData.str);
+   // loop through all the results and bind to the first we can
+   for(p = servinfo; p != NULL; p = p->ai_next) {
+      if ((sockfd = socket(p->ai_family, p->ai_socktype,
+            p->ai_protocol)) == -1) {
+         perror("listener: socket");
+         continue;
       }
 
-		if (receiveData.op == 0xAA) {
-			dvPacket_t sendPacket;
-         sendPacket.length = receiveData.length
-         sendPacket.id = receiveData.id;
-			sendPacket.dv = disemvowel(receiveData.str);
+      if (bind(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+         close(sockfd);
+         perror("listener: bind");
+         continue;
       }
 
-      sendData = (char)sendPacket;
+      break;
+   }
 
-		if ((numbytesSent = sendto(sockfd, sendData, sizeOf sendData, 0,
-      	   (struct sockaddr *)&their_addr, addr_len))==-1) {
-			perror("serverUDP: sendto");
-			exit(1);
-		}
+   if (p == NULL) {
+      fprintf(stderr, "listener: failed to bind socket\n");
+      return 2;
+   }
 
+   freeaddrinfo(servinfo);
 
+   while(1) {
+      printf("listener: waiting to recvfrom...\n");
 
-	}
-	close(sockfd);
+      addr_len = sizeof their_addr;
+      if ((numbytesRecv = recvfrom(sockfd, buf, MAXBUFLEN-1 , 0,
+         (struct sockaddr *)&their_addr, &addr_len)) ==-1)           {
+         perror("recvfrom");
+         exit(1);
+      }
 
-	return 0;
+      printf("listener: got packet from %s\n",
+         inet_ntop(their_addr.ss_family,
+            get_in_addr((struct sockaddr *)&their_addr),
+            their_addr_str, sizeof their_addr_str));
+
+      request.length = (buf[0] << 8) | buf[1];
+      request.id = (buf[2] << 8) | buf[3];
+      request.op = buf[4];
+
+      if (request.op == 0x55) {
+			printf("this is working");
+			nvResponse_t response;
+         response.length = 6;
+			packetSize = 6;
+			response.id = request.id;
+			response.numV = numVowels(&buf[5], request.length-5);
+			sendData = (char*) &response;
+			
+      } else if (request.op = 0xAA) {
+			dvResponse_t response;
+         disemvowel(&buf[5], message, request.length-5);
+			response.length = 4 + strlen(message);
+			packetSize = response.length;
+			response.id = request.id;
+			response.message = message;
+			sendData = (char*) &response;
+      }
+			printf("length %u %u", sendData[0], sendData[1]);
+			printf("id %u %u", sendData[2], sendData[3]);
+			printf("num vowels %u %u", sendData[4], sendData[5]);
+      
+
+      if ((numbytesSent = sendto(sockfd, &sendData, packetSize, 0,
+         (struct sockaddr *)&their_addr, addr_len))==-1) {
+         perror("serverUDP: sendto");
+         exit(1);
+      }
+   }
+   close(sockfd);
+
+   return 0;
 }
