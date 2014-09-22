8668e3f92b97a423aa48c6ee66013945eb1dc3a7
Ian <Typhoan@users.noreply.github.com>
2014-09-22 07:58:30 -0500

    added get host by name

--- Lab1/ClientTCP.c
+++ Lab1/ClientTCP.c
@@ -68,7 +68,7 @@ int main(int argc, char**argv)
 
    bzero(&servaddr,sizeof(servaddr));
    servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr=inet_addr(argv[1]);
+   servaddr.sin_addr.s_addr=inet_addr(gethostbyname(argv[1]));
    servaddr.sin_port=htons(PORTNUM);
 
    connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
