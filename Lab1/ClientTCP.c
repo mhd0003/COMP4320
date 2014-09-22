04002ab7e728e81d1c17e3dce653fc366a33aaae
Ian <Typhoan@users.noreply.github.com>
2014-09-22 15:56:27 -0500

    Update ClientTCP.c

--- Lab1/ClientTCP.c
+++ Lab1/ClientTCP.c
@@ -1,14 +1,16 @@
 /* Sample TCP client */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <sys/time.h>
+#include <netdb.h> 
 
-#define PORTNUM 10038
-
+#define PORT 10038
+  
 void packSendData(char* buf, uint16_t length, uint16_t id, uint8_t op, char* message) {
    int i;
    buf[0] = (length & 0xFF00) >> 8;
@@ -38,56 +40,90 @@ void getMessage(char* buf, char* message) {
 	}
 }
 
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+   if (sa->sa_family == AF_INET) {
+      return &(((struct sockaddr_in*)sa)->sin_addr);
+   }
 
-int main(int argc, char**argv)
+   return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
+int main(int argc, char **argv)
 {
 	double startTime;
-   int sockfd,nBytes;
-   struct sockaddr_in servaddr,cliaddr;
+   int sockfd, nBytes, rv;
+   struct sockaddr_in serveraddr;
+	struct hostent *server;
    char sendData[1029];
    char recvData[1029];
 	uint16_t length = 0;
 	uint16_t id;
 	uint8_t op;
-	char* message;
+	char *message;
+	char *hostname;
 	char dv[1024];
+	char *port = "10038";
 	
    if (argc != 5)
    {
       printf("usage:  ClientTCP hostname ID OP Message\n");
       exit(1);
    }
+	hostname = argv[1];
+
+	/* create the socket*/
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		error("Error opening socket");
 	
-   id = (uint16_t) (int) argv[2];
-   op = (uint8_t) (int) argv[3];
-   message = (char*)argv[4];
-   length = strlen(message) + 5;
-   packSendData(sendData, length, id, op, message);
-		
-   sockfd=socket(AF_INET,SOCK_STREAM,0);
+	/* gethostbyname: get server's DNS entry */
+	server = gethostbyname(hostname);
+	if (server == NULL) {
+		fprintf(stderr, "ERROR, no such host as %s\n", hostname);
+		exit(0);
+	}
+	
+	/* build the server's Internet address */
+	bzero((char *) &serveraddr, sizeof(serveraddr));
+    serveraddr.sin_family = AF_INET;
+    bcopy((char *)server->h_addr, 
+	  (char *)&serveraddr.sin_addr.s_addr, server->h_length);
+    serveraddr.sin_port = htons(PORT);
+	
+	/* connect: create a connection with the server */
+	if (connect(sockfd, &serveraddr, sizeof(serveraddr)) < 0) 
+      error("ERROR connecting");
 
-   bzero(&servaddr,sizeof(servaddr));
-   servaddr.sin_family = AF_INET;
-   servaddr.sin_addr.s_addr=inet_addr(gethostbyname(argv[1]));
-   servaddr.sin_port=htons(PORTNUM);
-
-   connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-
-   while (fgets(sendData, 10000,stdin) != NULL)
-   {
+	
+	id = (uint16_t) atoi(argv[2]);
+	op = (uint8_t) atoi(argv[3]);
+	message = argv[4];
+	length = strlen(message) + 5;
+	printf("Length: %u, ID: %u, OP: %u, Message: %s \n", length, id, op, message);
+	bzero(sendData, 1029);
+	packSendData(sendData, length, id, op, message);
+	
 		//startTime = Now();
-      sendto(sockfd,sendData,strlen(sendData),0,
-         (struct sockaddr *)&servaddr,sizeof(servaddr));
-      nBytes=recv(sockfd,recvData,1029,0);
+     	nBytes = write(sockfd, sendData, length);
+		if (nBytes < 0)
+			error("ERROR writing to socket");
+			
+		bzero(sendData, 1029);
+      nBytes = read(sockfd,recvData,1029);
+		if (nBytes < 0)
+			error("ERROR reading from socket");
 		
       //startTime = Now() - startTime;
 		
-	//printf("Time taken: %f\n", startTime);
-	printf("Request ID: %s\n", getID(recvData));
-	if (op == 55)
-		printf("Number of vowels: %u\n", getVowels(recvData));
-	else if (op == 170)
-		getMessage(recvData, dv);
-		printf("Response: %s\n",dv);
-   }
+		//printf("Time taken: %f\n", startTime);
+		printf("Request ID: %s\n", getID(recvData));
+		if (op == 55)
+			printf("Number of vowels: %u\n", getVowels(recvData));
+		else if (op == 170)
+			getMessage(recvData, dv);
+			printf("Response: %s\n",dv);
+
 }
